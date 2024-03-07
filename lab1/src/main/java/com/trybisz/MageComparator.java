2b61dc1e4c904a48860c97e964b54580aa7d5d52
Piotr Trybisz <ptrybisz@gmail.com>
2024-03-07 12:19:02 +0100

    Added descendants stats

--- lab1/src/main/java/com/trybisz/MageComparator.java
+++ lab1/src/main/java/com/trybisz/MageComparator.java
@@ -6,7 +6,7 @@ public class MageComparator implements Comparator<Mage> {
     @Override
     public int compare(Mage o1, Mage o2) {
 
-        if(o1.getLevel() != o2.getLevel()) return Integer.compare(o1.getLevel(), o2.getLevel());
+        if(o1.getLevel() != o2.getLevel()) return -Integer.compare(o1.getLevel(), o2.getLevel());
         if(o1.getName().compareTo(o2.getName()) != 0) return o1.getName().compareTo(o2.getName());
 
         return Double.compare(o1.getPower(), o2.getPower());
