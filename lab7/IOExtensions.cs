d9201019713ad7dd94a7b9dd617c6c45a9113587
Piotr Trybisz <ptrybisz@gmail.com>
2024-04-25 01:20:20 +0200

    Add lab 7 - TODO: Serialization

---
+++ lab7/IOExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System.Text;
+
+namespace MyProject
+{
+    internal static class IOExtensions
+    {
+
+        public static FileInfo FindOldestElement( this DirectoryInfo directoryInfo ) {
+            var directiories = directoryInfo.GetDirectories();
+            var files = directoryInfo.GetFiles();
+            var oldestElement = files.FirstOrDefault();
+            foreach (DirectoryInfo subDirectory in directiories)
+            {
+                var oldestFileInSubDirectory = subDirectory.FindOldestElement();
+                if (oldestElement == null || oldestFileInSubDirectory.LastWriteTime < oldestElement.LastWriteTime)
+                {
+                    oldestElement = oldestFileInSubDirectory;
+                }
+            }
+            return oldestElement;
+        }
+
+        public static string GetDOSAttributes( this FileSystemInfo fileSystemInfo ) {
+            var attributes = new StringBuilder();
+            attributes.Append((fileSystemInfo.Attributes & FileAttributes.ReadOnly) == FileAttributes.ReadOnly ? "R" : "-");
+            attributes.Append( (fileSystemInfo.Attributes & FileAttributes.Archive ) == FileAttributes.Archive ? "A" : "-" );
+            attributes.Append((fileSystemInfo.Attributes & FileAttributes.Hidden) == FileAttributes.Hidden ? "H" : "-");
+            attributes.Append( (fileSystemInfo.Attributes & FileAttributes.System ) == FileAttributes.System ? "S" : "-" );
+            
+            return attributes.ToString();
+        }
+    }
+}
---
+++ lab7/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace MyProject;
+class Program
+{
+    static void Main( string[] args ) {
+        var path = args[0];
+        //Console.WriteLine( $"Path: {path}" );
+        var directory = new DirectoryInfo( path );
+        PrintPathContents( directory );
+        var oldestElement = directory.FindOldestElement();
+        Console.WriteLine( $"Oldest element: {oldestElement.FullName} {oldestElement.LastWriteTime}" );
+
+        var allElements = directory.EnumerateFileSystemInfos()
+            .Select(x => new { Name = x.Name, SizeOrCount = x.GetType() == typeof(DirectoryInfo) ? ((DirectoryInfo)x).EnumerateFileSystemInfos().Count() : ((FileInfo)x).Length })
+            .OrderBy( x => x.Name.Length )
+            .ThenBy( x => x.Name )
+            .ToDictionary( x => x.Name, x => x.SizeOrCount );
+    }
+
+    static void PrintPathContents( DirectoryInfo directory, int depth = 0 ) {
+        foreach (var subDirectory in directory.GetDirectories())
+        {
+            Console.WriteLine($"{new string(' ', depth * 4)}{subDirectory.Name} {subDirectory.EnumerateFileSystemInfos().Count()} {subDirectory.GetDOSAttributes()}");
+            PrintPathContents(subDirectory, depth + 1);
+        }
+
+        foreach ( var file in directory.GetFiles() )
+        {
+            Console.WriteLine( $"{new string( ' ', depth * 4)}{file.Name} {file.Length}B {file.GetDOSAttributes()}" );
+        }
+    }
+}
